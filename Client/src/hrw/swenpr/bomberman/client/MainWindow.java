e4f2e047c270c44dce95abcf7be5f27300dec3c7
Marco Egger <eggerm@hs-weingarten.de>
2014-05-05 11:18:04 +0200

    First commit to set up repo with both projects (server and client)
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

---
+++ Client/src/hrw/swenpr/bomberman/client/MainWindow.java
@@ -0,0 +1,12 @@
+package hrw.swenpr.bomberman.client;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Server/src/hrw/swenpr/bomberman/server/MainWindow.java
@@ -0,0 +1,12 @@
+package hrw.swenpr.bomberman.server;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
