dda277a55029eaec97ddfbe874111edefce53b9d
Marco Egger <eggerm@hs-weingarten.de>
2014-05-05 11:28:49 +0200

    Added 'serialVersionUID' for MainWindow classes. Project is now ready to start!
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

--- Client/src/hrw/swenpr/bomberman/client/MainWindow.java
+++ Client/src/hrw/swenpr/bomberman/client/MainWindow.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 
 public class MainWindow extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
