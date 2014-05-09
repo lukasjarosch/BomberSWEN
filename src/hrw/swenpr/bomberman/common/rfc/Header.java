f6cba6178ccab51d0477349702f4bf7aa608d580
Marco Egger <eggerm@hs-weingarten.de>
2014-05-09 14:28:17 +0200

    Created Header class for data packets.
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

---
+++ src/hrw/swenpr/bomberman/common/rfc/Header.java
@@ -0,0 +1,25 @@
+package hrw.swenpr.bomberman.common.rfc;
+
+/**
+ * Header for every data packet send over socket. It contains the type of the message. Every other packet extends this class.
+ * 
+ * @author Marco Egger
+ */
+public class Header {
+	protected int type;
+
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+
+}
