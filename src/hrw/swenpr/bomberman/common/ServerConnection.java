202aa2ca9e1fbbbe606b6fbed7ba536f6150a306
Marco Egger <eggerm@hs-weingarten.de>
2014-05-09 20:02:02 +0200

    Implemented accepting from new connections on server side.
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

--- src/hrw/swenpr/bomberman/common/ServerConnection.java
+++ src/hrw/swenpr/bomberman/common/ServerConnection.java
@@ -1,5 +1,64 @@
 package hrw.swenpr.bomberman.common;
 
-public class ServerConnection extends Thread {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Provides an {@link ServerSocket} for accepting new connections on a specific port.
+ * 
+ * @author Marco Egger
+ */
+public class ServerConnection {
+	private ServerSocket server = null;
+	private Socket socket = null;
+	
+	
+	/**
+	 * Create a socket for a port.
+	 * 
+	 * @param port to listen on
+	 */
+	public ServerConnection(int port) {
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			// when creation failed print error and exit program
+			System.out.println("Unable to listen on port " + port);
+			System.exit(0);
+		}
+	}
+	
+	
+	/**
+	 * Waits until a new connection is established, then returns the new socket.
+	 * 
+	 * @return {@link Socket} the socket
+	 */
+	public Socket listenSocket() {
+		// reset socket
+		socket = null;
+		
+		// server.accept() blocks until a connection is established  
+		try {
+			socket = server.accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return socket;
+	}
+	
+	
+	/**
+	 * To close the {@code ServerSocket}. Should be called when terminating the software or no new connection should be accepted.
+	 */
+	public void closeServer() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
