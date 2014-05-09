6891ab7370b6ab0a09ee3d275c9d1af6bd9396dc
Marco Egger <eggerm@hs-weingarten.de>
2014-05-09 14:22:15 +0200

    Created client branch and added some comments.
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

--- src/hrw/swenpr/bomberman/client/MainClient.java
+++ src/hrw/swenpr/bomberman/client/MainClient.java
@@ -2,20 +2,40 @@ package hrw.swenpr.bomberman.client;
 
 import javax.swing.JFrame;
 
+/**
+ * <p>Main class containing {@code main()} function for starting client.</p>
+ * 
+ * <p>Extends {@link JFrame}.</p>
+ * 
+ * @author Marco Egger
+ * @author Daniel Hofer
+ */
 public class MainClient extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Starting function.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		new MainClient();
 	}
 	
+	
+	/**
+	 * Default constructor for main window.
+	 */
 	public MainClient() {
+		// setting up JFrame
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Bomberman");
 
+		// make window full-screen
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+		// position and show window
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
