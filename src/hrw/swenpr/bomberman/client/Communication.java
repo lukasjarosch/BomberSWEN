eecf41a9904310ed6efb0f3afbea41749d33ad08
Daniel <daniel.hofer@gmx.net>
2014-05-14 01:01:20 +0200

    Added communication methods for client

---
+++ src/hrw/swenpr/bomberman/client/Communication.java
@@ -0,0 +1,52 @@
+package hrw.swenpr.bomberman.client;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import hrw.swenpr.bomberman.common.ClientConnection;
+import hrw.swenpr.bomberman.common.rfc.*;
+
+
+/**
+ * <p> Class contains methods for communication 
+ * 
+ * @author Daniel Hofer
+ *
+ */
+public class Communication 
+{
+	//Socket for communication with Server
+	private Socket socket;
+	
+	public Communication()
+	{
+		try 
+		{
+			this.socket = ClientConnection.getSocket(InetAddress.getLocalHost().getHostName(), 6969);
+		} catch (UnknownHostException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Sends a message to the server
+	 * @param message Message that is send 
+	 */
+	public <T> void sendMessage(T message)
+	{
+		ObjectOutputStream out;
+		try {
+			out = new ObjectOutputStream(socket.getOutputStream());
+			out.writeObject(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
