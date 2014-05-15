0f737f3955a96226dbb0c5a2460ec6c96c0c5a7a
Daniel <daniel.hofer@gmx.net>
2014-05-15 16:33:36 +0200

    Updated class-diagramm

--- src/hrw/swenpr/bomberman/client/Field.java
+++ src/hrw/swenpr/bomberman/client/Field.java
@@ -3,15 +3,16 @@ package hrw.swenpr.bomberman.client;
 import hrw.swenpr.bomberman.common.rfc.Bomb;
 import hrw.swenpr.bomberman.common.rfc.Level;
 import hrw.swenpr.bomberman.common.rfc.User;
-import hrw.swenpr.bomberman.server.MainServer;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.io.File;
 
 import javax.swing.JPanel;
 
+
 /**
- * Provides the field and alld correspondent methods 
+ * Provides the field and all correspondent methods 
  * @author Daniel Hofer
  *
  */
@@ -21,6 +22,7 @@ public class Field extends JPanel
 	private Image field;
 	private Image Stone;
 	private Image bomb[];
+	private Image indestructible;
 	private int grid[][];
 	
 	
@@ -34,7 +36,7 @@ public class Field extends JPanel
 	 * @param members players with their position and color
 	 * @param bombs position and kind of bomb
 	 */
-	public void drawField(User members[], Bomb bombs[])
+	public void drawField(User members[], Bomb bombs[], File level)
 	{
 		
 	}
