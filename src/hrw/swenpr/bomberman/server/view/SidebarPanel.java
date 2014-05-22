5aaf523f05fc4b72f7a0f1be33a8bd56ec89558d
lukasjarosch <lukas.jarosch@mail.com>
2014-05-22 15:10:42 +0200

    Logging area is now visible

---
+++ src/hrw/swenpr/bomberman/server/view/SidebarPanel.java
@@ -0,0 +1,18 @@
+package hrw.swenpr.bomberman.server.view;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class SidebarPanel extends JPanel {
+	
+	/**
+	 * {@link SidebarPanel} constructor
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	public SidebarPanel() {
+		setBackground(Color.BLACK);
+	}
+
+}
