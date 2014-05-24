2fda80e656d6f67ed4bfd11149ac63d1e26b4ff3
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 10:43:17 +0200

    Added comments which describe the basic flow of the application

--- src/hrw/swenpr/bomberman/server/ServerMain.java
+++ src/hrw/swenpr/bomberman/server/ServerMain.java
@@ -1,6 +1,6 @@
 package hrw.swenpr.bomberman.server;
 
-import hrw.swenpr.bomberman.server.view.MainWindow;
+import hrw.swenpr.bomberman.server.ClientThread;
 
 import javax.swing.JFrame;
 
@@ -8,30 +8,58 @@ public class ServerMain extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Instance of the main window frame
-	 */
-	private MainWindow mainWindow = null;
-	
-	/**
-	 * Main method to start the server application
-	 * 
-	 * @author Lukas Jarosch
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		new ServerMain();
 	}
-
+	
+	public ServerMain() {
+		
+		// Initiate main frame
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Bomberman");
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		// Wait for logins until the game is started or 4 players are logged in
+		while(acceptLogin()) {
+			addPlayer();
+		}
+	}
+	
 	/**
-	 * Bootstrap the server application
+	 * Waits for a login message
+	 * The message is then processed and a 
+	 * {@link ClientThread} is created
+	 * 
+	 * @author Lukas Jarosch
+	 * 
+	 * @return Whether to accept further logins or not
+	 */
+	private boolean acceptLogin() {
+		
+		// Test if the game is already running => FALSE
+		// If game is running: Log: "Game already running. Login rejected"
+		
+		// Test how many players are logged in => FALSE
+		// If 4 players are already logged in: Log: "Too many players. Login rejected"
+		
+		// Player is ok to join 
+		// => TRUE
+	}
+	
+	/**
+	 * Handles the creation of a new player
 	 * 
 	 * @author Lukas Jarosch
 	 */
-	public ServerMain() {
+	private void addPlayer() {
 		
-		// Create MainWindow
-		mainWindow = new MainWindow();	
+		// Log: "PLAYERNAME has requested a login. Processing..."
+		
+		// Add player to the model
+		
+		// Create ClientThread
 	}
 
 }
