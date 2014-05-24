2bf8223a0a268b633cfc7cd1cf70655751c20cd1
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 10:22:09 +0200

    Added login method stubs

--- src/hrw/swenpr/bomberman/client/MainClient.java
+++ src/hrw/swenpr/bomberman/client/MainClient.java
@@ -1,5 +1,7 @@
 package hrw.swenpr.bomberman.client;
 
+import hrw.swenpr.bomberman.server.ClientThread;
+
 import javax.swing.JFrame;
 
 public class MainClient extends JFrame {
@@ -11,13 +13,53 @@ public class MainClient extends JFrame {
 	}
 	
 	public MainClient() {
+		
+		// Initiate main frame
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Bomberman");
-
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-
 		setLocationRelativeTo(null);
 		setVisible(true);
+		
+		// Wait for logins until the game is started or 4 players are logged in
+		while(acceptLogin()) {
+			addPlayer();
+		}
+	}
+	
+	/**
+	 * Waits for a login message
+	 * The message is then processed and a 
+	 * {@link ClientThread} is created
+	 * 
+	 * @author Lukas Jarosch
+	 * 
+	 * @return Whether to accept further logins or not
+	 */
+	private boolean acceptLogin() {
+		
+		// Test if the game is already running => FALSE
+		// If game is running: Log: "Game already running. Login rejected"
+		
+		// Test how many players are logged in => FALSE
+		// If 4 players are already logged in: Log: "Too many players. Login rejected"
+		
+		// Player is ok to join 
+		// => TRUE
+	}
+	
+	/**
+	 * Handles the creation of a new player
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	private void addPlayer() {
+		
+		// Log: "PLAYERNAME has requested a login. Processing..."
+		
+		// Add player to the model
+		
+		// Create ClientThread
 	}
 
 }
