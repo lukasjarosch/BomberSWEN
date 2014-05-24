f01796b6f261bd56efca29155d628fce630a1b05
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 18:26:27 +0200

    Added a login thread which will handle all login requests

---
+++ src/hrw/swenpr/bomberman/server/LoginThread.java
@@ -0,0 +1,62 @@
+package hrw.swenpr.bomberman.server;
+
+public class LoginThread implements Runnable {
+
+	/**
+	 * The login loop which is constantly waiting for a login request
+	 * by a client.
+	 * It is then processed and a response is sent
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	@Override
+	public void run() {
+		
+		while(ServerMain.isRunning()) {
+			
+			// Check for login request
+			
+			// acceptLogin()
+			
+			// Depending on the retVal of acceptLogin => addPlayer / sendError
+		}
+		
+		// Kill tha bitch
+	}
+
+	/**
+	 * Waits for a login message
+	 * The message is then processed and a 
+	 * {@link ClientThread} is created
+	 * 
+	 * @author Lukas Jarosch
+	 * 
+	 * @return Whether to accept further logins or not
+	 */
+	private boolean acceptLogin() {
+		
+		// Test if the game is already running => FALSE
+		// If game is running: Log: "Game already running. Login rejected"
+		
+		// Test how many players are logged in => FALSE
+		// If 4 players are already logged in: Log: "Too many players. Login rejected"
+		
+		// Player is ok to join 
+		// => TRUE
+		return true;
+	}
+	
+	/**
+	 * Handles the creation of a new player
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	private void addPlayer() {
+		
+		// Log: "PLAYERNAME has requested a login. Processing..."
+		
+		// Add player to the model
+		
+		// Create ClientThread
+	}
+}
