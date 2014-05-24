2fbe5e1fd7c414658231d6113c05b6fe84be5abe
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 18:35:29 +0200

    Added the readyCount

--- src/hrw/swenpr/bomberman/server/ServerModel.java
+++ src/hrw/swenpr/bomberman/server/ServerModel.java
@@ -8,6 +8,18 @@ import hrw.swenpr.bomberman.common.rfc.User;
 
 public class ServerModel extends BombermanBaseModel {
 
+	/**
+	 * How many players are ready to start the game?
+	 * If this matches the total player count the game will start
+	 */
+	private int readyCount = 0;
+	
+	/**
+	 * The {@link ServerModel} constructor
+	 * 
+	 * @param The players participating in the game
+	 * @param The level file to load
+	 */
 	public ServerModel(ArrayList<User> users, File level) {
 		super(users, level);
 	}
