276a4838232781501cfb33f105a4411c267acc7f
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 19:03:43 +0200

    Added the GameListener which implements the BombermanListener Interface

---
+++ src/hrw/swenpr/bomberman/server/GameListener.java
@@ -0,0 +1,21 @@
+package hrw.swenpr.bomberman.server;
+
+import hrw.swenpr.bomberman.common.BombEvent;
+import hrw.swenpr.bomberman.common.BombermanEvent;
+import hrw.swenpr.bomberman.common.BombermanListener;
+
+public class GameListener implements BombermanListener {
+
+	@Override
+	public void bombExplode(BombEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modelChanged(BombermanEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
