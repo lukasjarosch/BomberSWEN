ace2d5ae1f754c52f44736c1f2b7e7cddbf0586d
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 10:40:15 +0200

    Added handleLevelSelection() and handleTimeSelection()

--- src/hrw/swenpr/bomberman/server/ClientThread.java
+++ src/hrw/swenpr/bomberman/server/ClientThread.java
@@ -1,14 +1,37 @@
 package hrw.swenpr.bomberman.server;
 
+import hrw.swenpr.bomberman.common.rfc.LevelSelection;
+import hrw.swenpr.bomberman.common.rfc.TimeSelection;
+
 /**
  * This thread is started for each 
  * @author Lukas
  *
  */
 public class ClientThread implements Runnable {
+	
 
 	@Override
 	public void run() {		
 	}
 
+	/**
+	 * Handles a {@link LevelSelection} from the game admin(!)
+	 * Other players cannot select the level
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	public void handleLevelSelection() {
+		
+	}
+	
+	/**
+	 * Handles a {@link TimeSelection} from the game admin(!)
+	 * Other players cannot select the time
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	public void handleTimeSelection() {
+		
+	}
 }
