d51ce9cc3139cc2c3133c77f7005d9461b5e94f2
lukasjarosch <lukas.jarosch@mail.com>
2014-05-24 18:48:29 +0200

    Added handleUserReady()

--- src/hrw/swenpr/bomberman/server/ClientThread.java
+++ src/hrw/swenpr/bomberman/server/ClientThread.java
@@ -2,6 +2,7 @@ package hrw.swenpr.bomberman.server;
 
 import hrw.swenpr.bomberman.common.rfc.LevelSelection;
 import hrw.swenpr.bomberman.common.rfc.TimeSelection;
+import hrw.swenpr.bomberman.common.rfc.UserReady;
 
 /**
  * This thread is started for each 
@@ -10,7 +11,14 @@ import hrw.swenpr.bomberman.common.rfc.TimeSelection;
  */
 public class ClientThread implements Runnable {
 	
-
+	/**
+	 * If the player is the game admin
+	 */
+	private boolean gameAdmin = false;
+	
+	/**
+	 * @author Lukas Jarosch
+	 */
 	@Override
 	public void run() {		
 	}
@@ -33,5 +41,21 @@ public class ClientThread implements Runnable {
 	 */
 	public void handleTimeSelection() {
 		
+	}
+	
+	/**
+	 * Handles a {@link UserReady} message from the player
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	public void handleUserReady() {
+		
+		// Get the server model
+		
+		// model.increaseReadyCount();
+		
+		// Test if readyCount == playerCount
+		
+			// Start the game
 	}
 }
