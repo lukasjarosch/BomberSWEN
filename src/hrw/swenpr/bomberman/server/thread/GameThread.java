6746f84a05261c9b752edc72d41bcbf29c768599
lukasjarosch <lukas.jarosch@mail.com>
2014-05-25 13:30:35 +0200

    Added scheduleGameEnd() which will schedule the gameTimer

--- src/hrw/swenpr/bomberman/server/thread/GameThread.java
+++ src/hrw/swenpr/bomberman/server/thread/GameThread.java
@@ -1,9 +1,49 @@
 package hrw.swenpr.bomberman.server.thread;
 
+import hrw.swenpr.bomberman.server.ServerMain;
+import hrw.swenpr.bomberman.server.ServerModel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameThread implements Runnable {
 
+	private ServerModel model;
+	
+	/**
+	 * The {@link GameThread} constructor simply defines
+	 * the local 'model' variable to ease access to the server model
+	 * 
+	 * @author Lukas Jarosch
+	 * 
+	 */
+	public GameThread() {
+		model = ServerMain.getModel();
+	}
+	
+	/**
+	 * 
+	 */
 	@Override
 	public void run() {
 	}
 
+	/**
+	 * Sets up a {@link Timer} which will end the game
+	 * after the selected time.
+	 * 
+	 * The timer is stored in the {@link ServerModel}
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	private void scheduleGameEnd() {
+		model.setGameTimer(new Timer());
+		model.getGameTimer().schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				// End game
+			}
+		}, 1/* INSERT TIME */ );
+	}
 }
