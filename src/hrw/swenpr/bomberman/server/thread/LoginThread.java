e3300dca6f2fa144541db3694c6698cca7eacae3
lukasjarosch <lukas.jarosch@mail.com>
2014-05-25 12:44:06 +0200

    Improved login, added sendUserData()

--- src/hrw/swenpr/bomberman/server/thread/LoginThread.java
+++ src/hrw/swenpr/bomberman/server/thread/LoginThread.java
@@ -1,5 +1,9 @@
 package hrw.swenpr.bomberman.server.thread;
 
+import hrw.swenpr.bomberman.common.rfc.Login;
+import hrw.swenpr.bomberman.common.rfc.User;
+import hrw.swenpr.bomberman.server.LogMessage;
+import hrw.swenpr.bomberman.server.LogMessage.LEVEL;
 import hrw.swenpr.bomberman.server.ServerMain;
 
 public class LoginThread implements Runnable {
@@ -19,14 +23,21 @@ public class LoginThread implements Runnable {
 			
 			// Check for login request
 			
-			// acceptLogin()
+			if(requestLogin()) {
+				// Send LoginOK
+				ServerMain.getMainWindow().log(new LogMessage(LEVEL.INFORMATION, "Player {PLAYERNAME} logged in"));
+				// addPlayer( ... );
+			} else {
+				// Send ErrorMessage
+				ServerMain.getMainWindow().log(new LogMessage(LEVEL.INFORMATION, "Player {PLAYERNAME} was rejected"));
+			}
 			
 			// Depending on the retVal of acceptLogin => addPlayer / sendError
 		}
 		
 		// Kill tha bitch
 	}
-
+	
 	/**
 	 * Waits for a login message
 	 * The message is then processed and a 
@@ -36,7 +47,7 @@ public class LoginThread implements Runnable {
 	 * 
 	 * @return Whether to accept further logins or not
 	 */
-	private boolean acceptLogin() {
+	private boolean requestLogin() {
 		
 		// Test if the game is already running => FALSE
 		// If game is running: Log: "Game already running. Login rejected"
@@ -52,14 +63,29 @@ public class LoginThread implements Runnable {
 	/**
 	 * Handles the creation of a new player
 	 * 
+	 * @param Login message
+	 * 
 	 * @author Lukas Jarosch
 	 */
-	private void addPlayer() {
+	private void addPlayer(Login login) {
 		
 		// Log: "PLAYERNAME has requested a login. Processing..."
 		
 		// Add player model to the server model container
 		
 		// Create ClientThread
+	}
+	
+	/**
+	 * Sends a {@link User} object to all clients
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	private void sendUserData() {
+		
+		// Create User object
+		
+		// Send message to all connected clients
+		
 	}
 }
