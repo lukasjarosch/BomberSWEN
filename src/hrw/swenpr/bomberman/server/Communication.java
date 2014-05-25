9e6672c396afa06293d847a3db5fde33d40c3810
lukasjarosch <lukas.jarosch@mail.com>
2014-05-25 14:40:56 +0200

    Added Communication class

---
+++ src/hrw/swenpr/bomberman/server/Communication.java
@@ -0,0 +1,42 @@
+package hrw.swenpr.bomberman.server;
+
+import hrw.swenpr.bomberman.server.thread.ClientThread;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Communication {
+
+	/**
+	 * Constructor for the {@link Communication} class
+	 * 
+	 * @author Lukas Jarosch
+	 */
+	public Communication() {
+		
+	}
+	
+	/**
+	 * Send a message to a specified socket
+	 * 
+	 * @param socket The socket to use
+	 * @param message The message to send
+	 */
+	public <T> void sendToClient(Socket socket, T message) {
+		
+	}
+	
+	/**
+	 * Sends a message to all clients registered
+	 * 
+	 * @param message The message to send
+	 */
+	public <T> void sendToAllClients(T message){
+		ArrayList<ClientThread> threads = ServerMain.getModel().getClientThreads();
+		
+		while(threads.iterator().hasNext()) {
+			
+			// Send message to the socket: threads.iterator().next().getSocket();
+		}
+	}
+}
