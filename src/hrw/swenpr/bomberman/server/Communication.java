7fbcd69cc5de34439cfbd4d8ee4799314e3adf6c
lukasjarosch <lukas.jarosch@mail.com>
2014-05-28 18:46:05 +0200

    Added implementation for sendToClient() and sendToAllClients()

--- src/hrw/swenpr/bomberman/server/Communication.java
+++ src/hrw/swenpr/bomberman/server/Communication.java
@@ -1,9 +1,15 @@
 package hrw.swenpr.bomberman.server;
 
 import hrw.swenpr.bomberman.server.thread.ClientThread;
+import hrw.swenpr.bomberman.server.view.MainWindow;
+import hrw.swenpr.bomberman.server.LogMessage;
+import hrw.swenpr.bomberman.server.LogMessage.LEVEL;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
+
 
 public class Communication {
 	
@@ -14,11 +20,20 @@ public class Communication {
 	 * @param message The message to send
 	 */
 	public <T> void sendToClient(Socket socket, T message) {
+		ObjectOutputStream out;
 		
+		try {
+			out = new ObjectOutputStream(socket.getOutputStream());
+			out.writeObject(message);
+		} catch (IOException | NullPointerException e) {
+			e.printStackTrace();
+			MainWindow.log(new LogMessage(LEVEL.WARNING, "Unable to send message"));
+		}
 	}
 	
 	/**
-	 * Sends a message to all clients registered
+	 * Sends a message to all clients registered by iterating over
+	 * all ClientThreads and invoking sendToClient for each socket
 	 * 
 	 * @param message The message to send
 	 */
@@ -26,8 +41,7 @@ public class Communication {
 		ArrayList<ClientThread> threads = Server.getModel().getClientThreads();
 		
 		while(threads.iterator().hasNext()) {
-			
-			// Send message to the socket: threads.iterator().next().getSocket();
+			sendToClient(threads.iterator().next().getSocket(), message);
 		}
 	}
 }
