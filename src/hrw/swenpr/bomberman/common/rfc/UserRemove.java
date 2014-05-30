5651c40c672279454e1347a6fbf2ff41252a485a
Marco Egger <eggerm@hs-weingarten.de>
2014-05-30 11:03:14 +0200

    Extend RFC with UserRemove.
    Signed-off-by: Marco Egger <eggerm@hs-weingarten.de>

---
+++ src/hrw/swenpr/bomberman/common/rfc/UserRemove.java
@@ -0,0 +1,38 @@
+package hrw.swenpr.bomberman.common.rfc;
+
+import java.io.Serializable;
+
+/**
+ * <p> C <=> S
+ * {@link UserRemove} is sent from a client to the server, when he leaves the game/closes the software.
+ * The server forwards this message to the other clients to update the user tables.
+ * 
+ * @author Marco Egger
+ */
+public class UserRemove extends Header implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private int userID;
+
+	/**
+	 * @param userID
+	 */
+	public UserRemove(int userID) {
+		setType(MessageType.USER_REMOVE);
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+}
