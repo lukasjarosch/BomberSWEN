a683459673245bc2e94ff5c2da8a93c86e66f10a
Marco Egger <eggerm@hs-weingarten.de>
2014-06-25 23:33:38 +0200

    Added "except"-thread send function for forwarding messages.

--- src/hrw/swenpr/bomberman/server/Communication.java
+++ src/hrw/swenpr/bomberman/server/Communication.java
@@ -16,12 +16,11 @@ public class Communication {
 	 * 
 	 * @param socket The output stream to use
 	 * @param message The message to send
+	 * 
+	 * @author Lukas Jarosch
 	 */
 	public <T> void sendToClient(ObjectOutputStream out, T message) {
-		
 		try {
-			if(out == null)
-				System.out.println("out null");
 			out.writeObject(message);
 		} catch (IOException | NullPointerException e) {
 			e.printStackTrace();
@@ -34,12 +33,35 @@ public class Communication {
 	 * all ClientThreads and invoking sendToClient for each socket
 	 * 
 	 * @param message The message to send
+	 * 
+	 * @author Lukas Jarosch
 	 */
 	public <T> void sendToAllClients(T message){
 		ArrayList<ClientThread> threads = Server.getModel().getClientThreads();
 		
 		for (ClientThread clientThread : threads) {
 			sendToClient(clientThread.getOutputStream(), message);
+		}
+	}
+	
+	
+	/**
+	 * <p>Sends a message to all clients, except the {@code exceptThread}, by iterating
+	 * over all {@link ClientThread}s and invoking {@code sendToClient()} for each socket.
+	 * <p>Can be used to forward a message to all other clients.
+	 * 
+	 * @param message the message to send
+	 * @param exceptThread the {@link ClientThread} to be excluded from the "broadcast"
+	 * 
+	 * @author Marco Egger
+	 */
+	public <T> void sentToAllOtherClients(T message, ClientThread exceptThread) {
+		ArrayList<ClientThread> threads = Server.getModel().getClientThreads();
+
+		for (ClientThread clientThread : threads) {
+			// only send if exceptThread does not match
+			if(!clientThread.equals(exceptThread))
+				sendToClient(clientThread.getOutputStream(), message);
 		}
 	}
 }
